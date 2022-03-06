32a989b68c9f1796a8848cda163b23983ec806be
nilsreich <nilsreich@gmail.com>
2022-03-06 22:19:23 +0000

    i18n

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'de'],
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { NextIntlProvider } from 'next-intl';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <NextIntlProvider messages={pageProps.messages}>
+    <Component {...pageProps} />
+  </NextIntlProvider>
+  )
+}
+
+export default MyApp
